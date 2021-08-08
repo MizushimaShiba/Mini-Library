0f28af3b3c02e5ec4bbbd6dc7981d365a0462d5b
Mizushima Shiba <haidarme@yahoo.co.id>
2021-08-08 23:15:33 +0700

    fix wrong path

--- routes/students.js
+++ routes/students.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 const StudentsController = require('../controllers/students')
 
-router.get('/books', StudentsController.findAll)
-router.post('/books', StudentsController.create)
-router.put('/books/:id', StudentsController.updateName)
-router.get('/books/:id', StudentsController.findOne)
-router.delete('/books/:id', StudentsController.destroy)
+router.get('/students', StudentsController.findAll)
+router.post('/students', StudentsController.create)
+router.put('/students/:id', StudentsController.updateName)
+router.get('/students/:id', StudentsController.findOne)
+router.delete('/students/:id', StudentsController.destroy)
 
 
 
